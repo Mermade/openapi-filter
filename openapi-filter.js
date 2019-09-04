8c2cddbab7369494f3348c51766d73fc48cc893d
Birger Moell <birger.moell@bankgirot.se>
2019-09-04 14:18:35 +0200

    Added readme and used flag

--- openapi-filter.js
+++ openapi-filter.js
@@ -4,13 +4,15 @@
 
 const fs = require('fs');
 const yaml = require('js-yaml');
-const yargs = require('yargs');
 const openapiFilter = require('./index.js');
 
 let argv = require('yargs')
     .usage('Usage: openapi-filter [options] {infile} [{outfile}]')
     .demand(1)
     .strict()
+    .boolean('forcestyleliteral')
+    .describe('forcestyleliteral','keeps literal styling')
+    .alias('f','forcestyleliteral')
     .boolean('inverse')
     .describe('inverse','output filtered elements only')
     .alias('i','inverse')
@@ -18,20 +20,8 @@ let argv = require('yargs')
     .alias('t','tags')
     .describe('tags','tags to filter by')
     .default('tags',['x-internal'])
-    .array('overrides')
-    .alias('o', 'overrides')
-    .default('overrides', [])
-    .describe('overrides', 'tags to override fields')
     .boolean('valid')
     .describe('valid','try to ensure inverse output is valid')
-    .boolean('strip')
-    .alias('s', 'strip')
-    .describe('strip','strip the tags from the finished product')
-    .number('lineWidth')
-    .alias('l', 'lineWidth')
-    .describe('lineWidth','max line width of yaml output')
-    .default('lineWidth',-1)
-    .alias('i','inverse')
     .help('h')
     .alias('h', 'help')
     .version()
@@ -40,16 +30,12 @@ let argv = require('yargs')
 let s = fs.readFileSync(argv._[0],'utf8');
 let obj = yaml.safeLoad(s,{json:true});
 let res = openapiFilter.filter(obj,argv);
+
 if (argv._[0].indexOf('.json')>=0) {
     s = JSON.stringify(res,null,2);
 }
 else {
-    // forceStyleLiteral flag
-    if (argv._[2]) {
-        s = yaml.safeDump(res,{lineWidth:-1, forceStyleLiteral: true});
-    } else {
-        s = yaml.safeDump(res,{lineWidth:-1});
-    }
+    s = yaml.safeDump(res,{lineWidth:-1, forceStyleLiteral: argv.forcestyleliteral});
 }
 if (argv._.length>1) {
     fs.writeFileSync(argv._[1],s,'utf8');
