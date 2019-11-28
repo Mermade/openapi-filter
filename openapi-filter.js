efeb2e1446f56444f056cb0050e3b037c6b4ec7b
Mike Ralphson <mike.ralphson@gmail.com>
2019-11-28 14:11:51 +0000

    fix: yargs broke things, refs #21

--- openapi-filter.js
+++ openapi-filter.js
@@ -8,48 +8,50 @@ const {strOptions} = require('yaml/types');
 const openapiFilter = require('./index.js');
 
 let argv = require('yargs')
-    .usage('Usage: openapi-filter [options] {infile} [{outfile}]')
-    .demand(1)
+    .usage('$0 <infile> [outfile]',false,(yargs) => {
+      yargs
+        .positional('infile',{ describe: 'the input file' })
+        .positional('outfile',{ describe: 'the output file' })
+    })
     .strict()
     .boolean('info')
     .describe('info','include complete info object with --valid')
     .boolean('inverse')
     .describe('inverse','output filtered elements only')
-    .alias('i','inverse')
+    .alias('inverse','i')
     .array('tags')
-    .alias('t','tags')
+    .alias('tags','t')
     .describe('tags','tags to filter by')
     .default('tags',['x-internal'])
     .array('overrides')
-    .alias('o', 'overrides')
+    .alias('overrides','o')
     .default('overrides', [])
     .describe('overrides', 'tags to override fields')
     .boolean('valid')
     .describe('valid','try to ensure inverse output is valid')
     .boolean('strip')
-    .alias('s', 'strip')
+    .alias('strip','s')
     .describe('strip','strip the tags from the finished product')
     .number('lineWidth')
-    .alias('l', 'lineWidth')
+    .alias('lineWidth','l')
     .describe('lineWidth','max line width of yaml output')
     .default('lineWidth',-1)
-    .help('h')
-    .alias('h', 'help')
+    .help()
     .version()
     .argv;
 
-let s = fs.readFileSync(argv._[0],'utf8');
+let s = fs.readFileSync(argv.infile,'utf8');
 let obj = yaml.parse(s);
 let res = openapiFilter.filter(obj,argv);
-if (argv._[0].indexOf('.json')>=0) {
+if (argv.infile.indexOf('.json')>=0) {
     s = JSON.stringify(res,null,2);
 }
 else {
     strOptions.fold.lineWidth = argv.lineWidth;
     s = yaml.stringify(res);
 }
-if (argv._.length>1) {
-    fs.writeFileSync(argv._[1],s,'utf8');
+if (argv.outfile) {
+    fs.writeFileSync(argv.outfile,s,'utf8');
 }
 else {
     console.log(s);
