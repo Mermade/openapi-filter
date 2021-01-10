e54f3d120114a8477b53497c6e1dfd46be256a83
Tim <>
2021-01-10 17:11:57 +0100

    Changed level from INFO to WARN and the verbose messages based on the received feedback

--- openapi-filter.js
+++ openapi-filter.js
@@ -55,7 +55,7 @@ let argv = require('yargs')
     .version()
     .argv;
 
-if (argv.verbose) console.info('=== OpenAPI document fltering started ===','\n')
+if (argv.verbose) console.warn('=== Document filtering started ===','\n')
 
 // apply options from config file if present
 if (argv && argv.configFile) {
@@ -72,7 +72,7 @@ if (argv && argv.configFile) {
         console.error(err)
     }
 }
-if (argv.verbose) console.info('IN File:      ' + argv.infile)
+if (argv.verbose) console.warn('IN File:      ' + argv.infile)
 
 let s = fs.readFileSync(argv.infile,'utf8');
 let obj = yaml.parse(s, {maxAliasCount: argv.maxAliasCount});
@@ -87,10 +87,10 @@ else {
 if (argv.outfile) {
     fs.writeFileSync(argv.outfile,s,'utf8');
 
-    if (argv.verbose) console.info('OUT File:     ' + argv.outfile)
+    if (argv.verbose) console.warn('OUT File:     ' + argv.outfile)
 }
 else {
     console.log(s);
 }
-if (argv.verbose) console.info('\x1b[32m%s\x1b[0m','\n','✅ OpenAPI document was filtered successful!')
+if (argv.verbose) console.warn('\n','✅ Document was filtered successfully!')
 
